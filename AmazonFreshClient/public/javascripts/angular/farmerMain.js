52e29a9390ed4682c32b17ab97e422c23f2eb7a6
Rahil Vora <rahil.vora740@gmail.com>
2016-04-25 01:25:42 -0700

    admin crud

--- AmazonFreshClient/public/javascripts/angular/farmerMain.js
+++ AmazonFreshClient/public/javascripts/angular/farmerMain.js
@@ -1,29 +1,98 @@
 var FarmerApp = angular.module("FarmerApp",["ngRoute"]);
 
-
+var editproductid=0;
 FarmerApp.controller("FarmerProductController", ["$scope", "$http", "$location", function($scope, $http, $location){
     $scope.farmerProducts = [];
     $scope.farmers = [];
 
+
     //Get all the farmers products
 
     $http.get('api/getFarmerProducts').then(function(result){
-        console.log("getFarmerProducts "+result.data);
+
         $scope.farmerProducts = result.data;
         //$location.path('/farmers');
     });
 
     //form to add a new product
-    $http.get('api/createProduct').then(function(result){
-        console.log("getFarmerProducts "+result.data);
-        $scope.farmerProducts = result.data;
-        //$location.path('/farmers');
-    });
 
+
+    $scope.addProduct= function(){
+        console.log($scope.form.price);
+
+
+        $http.post('api/createProduct', $scope.form).then(function(result){
+
+        });
+    }
+
+    $scope.getEditProduct= function(productid){
+
+        alert(productid);
+        editproductid=productid;
+        $location.path('/editProduct');
+      /*  $http.get('api/getEditProduct', {params:{data:productid}}).then(function(result){
+            if(result) {
+
+                alert(result.data[0].productname);
+                $location.path('/editProduct');
+
+            }
+
+        });*/
+    }
+
+   /* $scope.editProduct= function(){
+
+
+        $http.put('api/editProduct', $scope.form).then(function(result){
+
+        });
+    }*/
 
 
 }]);
 
+
+FarmerApp.controller("FarmerEditProfileController", ["$scope", "$http", "$location", function($scope, $http, $location){
+
+    $http.get('api/getEditProduct', {params:{data:editproductid}}).then(function(result){
+        if(result) {
+
+            alert("in " + result.data[0].productname);
+
+           // $scope.productname.value=result.data[0].productname;
+          //  $scope.editproductname=result.data[0].productname;
+           $scope.form.productname=result.data[0].productname;
+            $scope.form.productprice=result.data[0].productprice;
+            $scope.form.productdescription=result.data[0].description;
+
+
+        }
+
+    })
+
+}]);
+
+
+
+FarmerApp.controller("FarmerProfileController", ["$scope", "$http", "$location", function($scope, $http, $location){
+
+    $http.get('api/getFarmerProfile').then(function(result){
+
+        //fetch farmer info
+
+    });
+
+    $scope.editFarmerProfile= function() {
+
+
+        $http.put('api/editFarmerProfile', $scope.form).then(function (result) {
+
+        });
+    };
+
+}]);
 
 FarmerApp.config(['$routeProvider',
     function($routeProvider) {
@@ -36,7 +105,15 @@ FarmerApp.config(['$routeProvider',
             templateUrl: '../view/farmerViews/addNewProduct.ejs',
             controller : 'FarmerProductController'
         }).
+        when('/viewProfile',{
+            templateUrl: '../view/farmerViews/viewProfile.ejs',
+            controller : 'FarmerProfileController'
+        }).
+        when('/editProduct',{
+            templateUrl: '../view/farmerViews/editProduct.ejs',
+            controller : 'FarmerEditProfileController'
+        }).
         otherwise({
-            redirectTo: "/"
+            controller : 'defaultController'
         })
     }]);
