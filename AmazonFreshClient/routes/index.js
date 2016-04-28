c35a1664373afa06ed0fd0e6f36dc05598dc9c82
balajianoopgupta <balajianoopgupta.ba@gmail.com>
2016-04-27 22:42:53 -0700

    Changes to index.js file

--- AmazonFreshClient/routes/index.js
+++ AmazonFreshClient/routes/index.js
@@ -2,18 +2,21 @@ var express = require('express');
 var router = express.Router();
 
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('homepage');
 });
 
 router.get('/admin', function(req, res, next) {
-  res.render('admin', { title: 'Express' });
+  res.render('adminViews/admin');
 });
 
 router.get('/farmer', function(req, res, next) {
-  res.render('farmer', { title: 'Farmer' });
+  res.render('farmerViews/farmer');
 });
 
+router.get('/login',function (req,res,next) {
+  res.render('loginAndSignUp/login');
+});
 
-
+router.get('/')
 
 module.exports = router;
